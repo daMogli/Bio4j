0c4d50fe9b3eaca03b2c41933575577f90b6ef6b
daMogli <mogli@berkeley.edu>
2013-04-27 03:21:35 -0700

    Update DownloadAndPrepareBio4jSources.sh

--- DownloadAndPrepareBio4jSources.sh
+++ DownloadAndPrepareBio4jSources.sh
@@ -1,6 +1,5 @@
 curl 'ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.xml.gz' -o uniprot_sprot.xml.gz
 curl 'ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_trembl.xml.gz' -o uniprot_trembl.xml.gz
-curl 'ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot_varsplic.fasta.gz' -o uniprot_sprot_varsplic.fasta.gz
 curl 'http://archive.geneontology.org/latest-termdb/go_daily-termdb.obo-xml.gz' -o go.xml.gz
 curl 'ftp://ftp.ncbi.nih.gov/pub/taxonomy/gi_taxid_nucl.dmp.gz' -o gi_taxid_nucl.dmp.gz
 curl 'ftp://ftp.expasy.org/databases/enzyme/enzyme.dat' -o enzyme.dat
